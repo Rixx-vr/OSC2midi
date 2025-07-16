1994cdd73f26d7c96646e1f20ce9093364ca4a51
Rixx <rixx_vr@posteo.de>
2025-07-16 15:30:26 +0200

    bridge: add libs for midi and osc

---
+++ src-tauri/src/bridge.rs
@@ -0,0 +1,51 @@
+use rosc::{encoder, OscMessage, OscPacket, OscType};
+use midir::{MidiInput, MidiInputConnection, MidiInputPort, MidiOutput};
+use wmidi::{MidiMessage, Note};
+use std::error::Error;
+
+
+pub struct MidiManager{
+    pub midi_in: MidiInput,
+    midi_connections: Vec<MidiInputConnection<()>>,
+    pub midi_out: MidiOutput
+}
+
+impl MidiManager {
+    pub fn new() -> Result<Self, Box<dyn Error>> {
+        Ok(Self {
+            midi_in: MidiInput::new("MIDI Input")?,
+            midi_out: MidiOutput::new("MIDI Output")?,
+            midi_connections: Vec::new(),
+        })
+    }
+
+    pub fn list_devices(&self) {
+        for port in self.midi_in.ports() {
+            println!("Input port: {}", self.midi_in.port_name(&port).unwrap());
+        }
+
+        for port in self.midi_out.ports() {
+            println!("Output port: {}", self.midi_out.port_name(&port).unwrap());
+        }
+    }
+
+    pub fn connect(&mut self, port: &MidiInputPort) -> Result<(), Box<dyn Error>> {
+        let connection = MidiInput::new("Listener")?.connect(
+            &port,
+            "midir‑in",
+            |_, message, _| {
+                match MidiMessage::from_bytes(message) {
+                    Ok(msg) => println!("Received: {:?}", msg),
+                    Err(_) => println!("Raw bytes: {:?}", message),
+                }
+            },
+            (),
+        )?;
+
+        self.midi_connections.push(connection);
+
+        Ok(())
+    }
+}
+
+
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod bridge;
+
 fn main() {
+    let mut bridge = bridge::MidiManager::new().expect("Failed to create MIDI manager");
+
+    bridge.list_devices();
+
+    bridge.connect(bridge.midi_in.ports().first().expect("No MIDI input ports found"))
+        .expect("Failed to connect to MIDI input port");
+
     midi_osc_bridge_lib::run()
 }
